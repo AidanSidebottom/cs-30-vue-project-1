ab9e2362c19bfe5edb7bb11c6f73b44dd27a37bc
Aidan Sidebottom <1005083003@edu.cbe.ab.ca>
2022-12-05 11:56:03 -0700

    added peice names

--- src/components/board.vue
+++ src/components/board.vue
@@ -1,6 +1,7 @@
 <script setup>
 import{ref,computed} from 'vue'
-const player = ref('x')
+const player = ref('white')
+const color = ref('white')
 const board = ref([
   ['c','h','b','k','q','b','h','c'],
   ['p','p','p','p','p','p','p','p'],
@@ -8,10 +9,11 @@ const board = ref([
   ['','','','','','','',''],
   ['','','','','','','',''],
   ['','','','','','','',''],
-  ['','','','','','','',''],
   ['p','p','p','p','p','p','p','p'],
   ['c','h','b','k','q','b','h','c'],
 ])
+
+
 </script>
  
 <template>
@@ -26,9 +28,12 @@ const board = ref([
       <div 
         v-for="(cell, y) in row"
         :key="y"
-        class = "cell"
+        class = "white"
+
       >
-     
+        <div>
+        {{board[x][y]}}
+        </div>
       </div>
      </div>
   </main>
@@ -40,24 +45,39 @@ const board = ref([
 
 <style scoped>
 .board{
-  
+  margin: auto;
+  width: 50%;
 }
-.cell{
+.white{
   display:inline-block;
   vertical-align:middle;
-  background-color:lightgreen;
-  border: white;
-  border-radius: 10px;
-  height:10px;
-  width:10px;
-  background-color: rgb(126, 51, 51);
-  
+  border: 3px solid white;
+  border-radius: 3px;
+  height:20px;
+  width:20px;
+  background-color: rgb(81, 158, 115);
+  padding:10px;
+}
+.black{
+  display:inline-block;
+  vertical-align:middle;
+  border: 3px solid white;
+  border-radius: 3px;
+  height:20px;
+  width:20px;
+  background-color: rgb(3, 17, 9);
   padding:10px;
 }
 .player{
   padding: 10px;
   font-size: 20px;
-  
+  text-align: center;
+}
+.white:hover{
+  background-color: rgb(180, 76, 16);
+}
+.black:hover{
+  background-color: rgb(180, 76, 16);
 }
 
 </style>
